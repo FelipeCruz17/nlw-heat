50417f61dc30883e71e141dd61bd1d09c948fdfc
Felipe Cruz <feemattioli@gmail.com>
2021-11-03 21:03:32 -0300

    first commit: node from nlw-heat rocketseat

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: "token.invalid",
+    });
+  }
+
+  //Bearer 85709847509814759084759470580934584375890 posição [0]
+  // -7518475437574385793475 token posição [1]
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
+}
